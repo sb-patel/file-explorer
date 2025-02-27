5fcd4b0740aba08e625c1aa7e9bb1feb46ca3576
Sandeep <sandeep.patel4488@gmail.com>
2025-02-27 09:14:26 +0530

    Expand/Collapse functionality updated for all individual folders

--- src/List.jsx
+++ src/List.jsx
@@ -1,18 +1,24 @@
 import { useState } from "react";
-const [isExpanded, setIsExpanded] = useState(false);
 
-const List = ({ list }) => (
-    <div className="container">
-        {
-            list.map((node) => (
-                <div key={node.id}>
-                    {node.isFolder && <span onClick={() => setIsExpanded((prev) => !prev)}>+</span>}
-                    <span>{node.name}</span>
-                    {isExpanded && node.children && <List list={node.children} />}
-                </div>
-            ))
-        }
-    </div>
-)
+function List({ list }) {
+    const [isExpanded, setIsExpanded] = useState({});
+
+    return (
+        <div className="container">
+            {
+                list.map((node) => (
+                    <div key={node.id}>
+                        {node.isFolder && <span onClick={() => setIsExpanded((prev) => ({
+                            ...prev,
+                            [node.name]: !prev[node.name],
+                            }))}>{isExpanded?.[node.name] ? '-' : '+'}</span>}
+                        <span>{node.name}</span>
+                        {isExpanded?.[node.name] && node.children && <List list={node.children} />}
+                    </div>
+                ))
+            }
+        </div>
+    )
+};
 
 export default List;
