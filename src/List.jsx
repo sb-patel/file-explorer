46c77711bf8b3fc8404ae67ca395994efb1fba37
Sandeep <sandeep.patel4488@gmail.com>
2025-02-28 09:19:24 +0530

    file/folder add icon added

--- src/List.jsx
+++ src/List.jsx
@@ -13,6 +13,13 @@ function List({ list }) {
                             [node.name]: !prev[node.name],
                             }))}>{isExpanded?.[node.name] ? '-' : '+'}</span>}
                         <span>{node.name}</span>
+                        <span>
+                            <img
+                            src="https://cdn-icons-png.flaticon.com/512/5994/5994754.png"
+                            className="icon"
+                            />
+
+                        </span>
                         {isExpanded?.[node.name] && node.children && <List list={node.children} />}
                     </div>
                 ))
